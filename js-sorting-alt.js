ee9eb7301a6218f00a8708ca22b293e81086d4c6
Basov Leonid <basov.leonid@gmail.com>
2012-11-06 23:46:44 +0300

    heap sort

--- js-sorting-alt.js
+++ js-sorting-alt.js
@@ -44,3 +44,83 @@ function merge(left, right){
 		result = result.concat(right);
 	return result;
 }
+
+
+function bubble_sort(ar){
+	for(var i=0; i < ar.length; i++){
+		var founded = false;
+		for(var j=0; j < ar.length - i; j++){
+			if( ar[j] > ar[j+1] ){
+				swap( ar[j], ar[j+1]);
+				founded = true;
+			}
+		}
+		if(!founded)
+			break;
+	}
+}
+
+// function qsort(ar){
+
+// }
+
+// function _qSort(ar, low, high) {
+//       var i = low;
+//       var j = high;
+//       var x = A[ (low+high) / 2 ];
+//       do {
+//           while(ar[i] < x) ++i;
+//           while(ar[j] > x) --j;
+//           if(i <= j){
+//               int temp = ar[i];
+//               ar[i] = ar[j];
+//               ar[j] = temp;
+//               i++; j--;
+//           }
+//       } while(i <= j);
+ 
+//       if(low < j) qSort(ar, low, j);
+//       if(i < high) qSort(ar, i, high);
+//   }
+
+
+
+
+function heap_sort2(ar){
+	build_heap(ar);
+	for(var i = ar.length - 1; i > 0; i--){
+		my_swap(ar, 0, i);
+		heapify(ar, 0, i-1);
+	}
+	return ar;
+}
+
+function heapify(a, i, j){
+	var k;
+	if(2*i+1 > j) return;
+	if(2*i+2 > j) {
+		k = 2*i+1;
+	} else {
+		if(a[2*i+1] > a[2*i+2])
+			k = 2*i+1;
+		else
+			k = 2*i+2;
+	}
+	if(a[i] < a[k] ){
+		my_swap(a, i, k);
+		heapify(a, k, j);
+	}
+}
+
+function build_heap (a){
+	var mid = Math.floor(a.length / 2);
+	for(var i= mid-1; i >= 0; i--){
+		heapify(a, i, a.length - 1);
+	}
+}
+
+function my_swap(ar, i, j) {	
+    var tmp = ar[i];
+    ar[i] = ar[j];
+    ar[j] = tmp;
+}
